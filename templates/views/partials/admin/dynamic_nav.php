56971fb6537de440c97946e67f2f214eff2ad4bd
NicholoDesanti <nicholo.desanti@gmail.com>
2024-05-10 09:10:29 -0700

    Removed Broken link

--- templates/views/partials/admin/dynamic_nav.php
+++ templates/views/partials/admin/dynamic_nav.php
@@ -3,7 +3,6 @@
     <li><?= anchor('awards/manage', '<i class="fa fa-certificate"></i> Manage Awards') ?></li>
     <li><?= anchor('honorary_titles/manage', '<i class="fa fa-at"></i> Manage Honorary Titles') ?></li>
     <li><?= anchor('populace_members/manage', '<i class="fa fa-user"></i> Manage Populace Members') ?></li>
-    <li><?= anchor('populace_awards/manage', '<i class="fa fa-mortar-board"></i> Manage Populace Awards') ?></li>
     <li><?= anchor('crowns/manage', '<i class="fa fa-sun-o"></i> Manage Crowns') ?></li>
 
 </ul>
