fee5d85f88acabcef16efeef7a58cac50b3650ff
NicholoDesanti <nicholo.desanti@gmail.com>
2024-05-14 23:39:30 -0700

    Fixed nav

--- templates/views/partials/admin/dynamic_nav.php
+++ templates/views/partials/admin/dynamic_nav.php
@@ -1,11 +1,13 @@
 <ul>
-    <li><?= anchor('branches/manage', '<i class="fa fa-bank"></i> Manage Branches') ?></li>
-    <li><?= anchor('awards/manage', '<i class="fa fa-certificate"></i> Manage Awards') ?></li>
-    <li><?= anchor('honorary_titles/manage', '<i class="fa fa-at"></i> Manage Honorary Titles') ?></li>
-    <li><?= anchor('populace_members/manage', '<i class="fa fa-user"></i> Manage Populace Members') ?></li>
-    <li><?= anchor('crowns/manage', '<i class="fa fa-sun-o"></i> Manage Crowns') ?></li>
+    <li><?= anchor('branches/manage', '<i class="fa fa-bank"></i> Branches') ?></li>
+    <li><?= anchor('crowns/manage', '<i class="fa fa-sun-o"></i> Crowns') ?></li>
+    <li><?= anchor('awards/manage', '<i class="fa fa-certificate"></i> Awards') ?></li>
+    <li><?= anchor('honorary_titles/manage', '<i class="fa fa-at"></i> Honorary Titles') ?></li>
+    <li><?= anchor('officer_positions/manage', '<i class="fa fa-legal"></i> Officer Positions') ?></li>
+    <li><?= anchor('populace_members/manage', '<i class="fa fa-user"></i> Populace Members') ?></li>
+  
 
-    <li><?= anchor('populace_awards/manage', '<i class="fa fa-trophy"></i> Manage Populace Awards') ?></li>
-    <li><?= anchor('populace_honorarys/manage', '<i class="fa fa-header"></i> Populace Honorary Awards & Titles') ?></li>
-    <li><?= anchor('officer_positions/manage', '<i class="fa fa-legal"></i> Manage Officer Positions') ?></li>
+    <li><?= anchor('populace_awards/manage', '<i class="fa fa-trophy"></i> Populace Awards') ?></li>
+    <li><?= anchor('populace_honorarys/manage', '<i class="fa fa-header"></i>Honorary Awards & Titles') ?></li>
+
 </ul>
