e98aaa8bcfcf2537e6c1c276ed91c4eb8beb1046
NicholoDesanti <nicholo.desanti@gmail.com>
2024-05-15 23:12:37 -0700

    fixed Honorary

--- modules/populace_honorarys/views/create.php
+++ modules/populace_honorarys/views/create.php
@@ -2,21 +2,21 @@
 <?= validation_errors() ?>
 <div class="card">
     <div class="card-heading">
-        Populace Position Details
+        Populace Honorary Details
     </div>
     <div class="card-body">
         <?php
         echo form_open($form_location);
-        echo form_label('End Date');
-        $attr = array("class"=>"date-picker", "autocomplete"=>"off", "placeholder"=>"Select End Date");
-        echo form_input('end_date', $end_date, $attr);
+        echo form_label('Date Earned');
+        $attr = array("class"=>"date-picker", "autocomplete"=>"off", "placeholder"=>"Select Date Earned");
+        echo form_input('date_earned', $date_earned, $attr);
         echo form_label('Associated Branch');
         echo form_dropdown('branches_id', $branches_options, $branches_id, array('id' => 'branches_id'));
-        echo form_label('Associated Officer Position');
-        echo form_dropdown('officer_positions_id', $officer_positions_options, $officer_positions_id, array('id' => 'officer_positions_id'));
+        echo form_label('Honorary Title');
+        echo form_dropdown('honorary_titles_id', $honorary_titles_options, $honorary_titles_id, array('id' => 'honorary_titles_id'));
         echo form_label('Associated Crown');
         echo form_dropdown('crowns_id', $crowns_options, $crowns_id, array('id' => 'crowns_id'));
-        echo form_label('Associated Populace Member');
+        echo form_label('Populace Member');
         echo form_dropdown('populace_members_id', $populace_members_options, $populace_members_id);
         echo form_submit('submit', 'Submit');
         echo anchor($cancel_url, 'Cancel', array('class' => 'button alt'));
@@ -33,26 +33,26 @@ $(document).ready(function() {
 
         $.ajax({
             type: 'POST',
-            url: '<?= BASE_URL ?>populace_positions/get_officer_positions_by_branch',
+            url: '<?= BASE_URL ?>populace_honorarys/get_honorary_titles_by_branch',
             data: {branch_id: branch_id},
             success: function(response) {
-                var positions = JSON.parse(response);
-                var positionsDropdown = $('#officer_positions_id');
-                positionsDropdown.empty();
-                positionsDropdown.append('<option value="">Select...</option>');
+                var titles = JSON.parse(response);
+                var titlesDropdown = $('#honorary_titles_id');
+                titlesDropdown.empty();
+                titlesDropdown.append('<option value="">Select...</option>');
 
-                $.each(positions, function(key, value) {
-                    positionsDropdown.append($('<option></option>').attr('value', key).text(value));
+                $.each(titles, function(key, value) {
+                    titlesDropdown.append($('<option></option>').attr('value', key).text(value));
                 });
             },
             error: function() {
-                alert('Error fetching officer positions.');
+                alert('Error fetching honorary titles.');
             }
         });
 
         $.ajax({
             type: 'POST',
-            url: '<?= BASE_URL ?>populace_positions/get_crowns_by_branch',
+            url: '<?= BASE_URL ?>populace_honorarys/get_crowns_by_branch',
             data: {branch_id: branch_id},
             success: function(response) {
                 var crowns = JSON.parse(response);
